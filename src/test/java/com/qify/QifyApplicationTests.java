0e66d69f6fd03692e72dca8c119a042634f555cd
builtbyjuls <juliusclapugot@gmail.com>
2026-09-04 17:04:47 +0800

    [test] add PostgreSQL integration test foundation

--- src/test/java/com/qify/QifyApplicationTests.java
+++ src/test/java/com/qify/QifyApplicationTests.java
@@ -1,12 +1,37 @@
 package com.qify;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class QifyApplicationTests {
 
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18.6-alpine");
+
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoadsWithPostgreSql() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData metadata = connection.getMetaData();
+
+            assertEquals("PostgreSQL", metadata.getDatabaseProductName());
+            assertEquals(18, metadata.getDatabaseMajorVersion());
+        }
     }
 }
