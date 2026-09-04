96ddb05d0069a1bb59c26b3ad393b3e1a06343fe
builtbyjuls <juliusclapugot@gmail.com>
2026-09-04 14:27:59 +0800

    [chore] bootstrap Spring Boot application

---
+++ src/main/java/com/qify/QifyApplication.java
@@ -0,0 +1,12 @@
+package com.qify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QifyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/qify/QifyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.qify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
